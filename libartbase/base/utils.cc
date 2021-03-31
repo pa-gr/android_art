hopez13/art

Improve string splitting
String splitting is something that we often have to do but our support
code for doing so is not the best. Add support for using
std::string_view in many circumstances and add support for making an
iterator of splits without allocation.

Test: ./test.py --host
Change-Id: I1b56b7e10926a064b64011326b508dd4af707df9

libartbase/base/utils.cc
@@ -25,11 +25,13 @@
 
 #include <fstream>
 #include <memory>
+#include <string>
 
 #include "android-base/file.h"
 #include "android-base/stringprintf.h"
 #include "android-base/strings.h"
 
+#include "base/stl_util.h"
 #include "bit_utils.h"
 #include "os.h"
 
@@ -230,21 +232,42 @@ std::string PrettySize(uint64_t byte_count) {
                       byte_count / kBytesPerUnit[i], kUnitStrings[i]);
 }
 
-void Split(const std::string& s, char separator, std::vector<std::string>* result) {
-  const char* p = s.data();
-  const char* end = p + s.size();
-  while (p != end) {
-    if (*p == separator) {
-      ++p;
-    } else {
-      const char* start = p;
-      while (++p != end && *p != separator) {
-        // Skip to the next occurrence of the separator.
-      }
-      result->push_back(std::string(start, p - start));
+template <typename StrIn, typename Str>
+void Split(const StrIn& s, char separator, std::vector<Str>* result) {
+  for (std::string_view p : SplitString(std::string_view(s), separator)) {
+    if (p.empty()) {
+      continue;
     }
+    result->push_back(Str(p));
   }
 }
+
+template void Split(const char *const& s, char separator, std::vector<std::string>* result);
+template void Split(const std::string& s, char separator, std::vector<std::string>* result);
+template void Split(const char *const& s, char separator, std::vector<std::string_view>* result);
+template void Split(const std::string_view& s,
+                    char separator,
+                    std::vector<std::string_view>* result);
+
+template <typename Str>
+void Split(const Str& s, char separator, size_t len, Str* result) {
+  Str* last = result + len;
+  for (std::string_view p : SplitString(std::string_view(s), separator)) {
+    if (p.empty()) {
+      continue;
+    }
+    if (result == last) {
+      return;
+    }
+    *result++ = Str(p);
+  }
+}
+
+template void Split(const std::string& s, char separator, size_t len, std::string* result);
+template void Split(const std::string_view& s,
+                    char separator,
+                    size_t len,
+                    std::string_view* result);
 
 void SetThreadName(const char* thread_name) {
   bool hasAt = false;

libartbase/base/utils.h
@@ -44,7 +44,17 @@ std::string PrettySize(uint64_t size_in_bytes);
 
 // Splits a string using the given separator character into a vector of
 // strings. Empty strings will be omitted.
-void Split(const std::string& s, char separator, std::vector<std::string>* result);
+template<typename StrIn, typename Str>
+void Split(const StrIn& s, char separator, std::vector<Str>* result);
+
+template<typename Str>
+void Split(const Str& s, char separator, size_t len, Str* result);
+
+
+template<typename StrIn, typename Str, size_t kLen>
+void Split(const StrIn& s, char separator, std::array<Str, kLen>* result) {
+  Split<Str>(Str(s), separator, kLen, &((*result)[0]));
+}
 
 // Returns the calling thread's tid. (The C libraries don't expose this.)
 uint32_t GetTid();

pa-gr/android_art

libartbase/base/utils.cc
@@ -25,11 +25,13 @@
 
 #include <fstream>
 #include <memory>
+#include <string>
 
 #include "android-base/file.h"
 #include "android-base/stringprintf.h"
 #include "android-base/strings.h"
 
+#include "base/stl_util.h"
 #include "bit_utils.h"
 #include "os.h"
 
@@ -235,21 +237,44 @@ std::string PrettySize(int64_t byte_count) {
                       negative_str, byte_count / kBytesPerUnit[i], kUnitStrings[i]);
 }
 
-void Split(const std::string& s, char separator, std::vector<std::string>* result) {
-  const char* p = s.data();
-  const char* end = p + s.size();
-  while (p != end) {
-    if (*p == separator) {
-      ++p;
-    } else {
-      const char* start = p;
-      while (++p != end && *p != separator) {
-        // Skip to the next occurrence of the separator.
-      }
-      result->push_back(std::string(start, p - start));
+template <typename StrIn, typename Str>
+void Split(const StrIn& s, char separator, std::vector<Str>* out_result) {
+  auto split = SplitString(std::string_view(s), separator);
+  for (std::string_view p : split) {
+    if (p.empty()) {
+      continue;
     }
+    out_result->push_back(Str(p));
   }
 }
+
+template void Split(const char *const& s, char separator, std::vector<std::string>* out_result);
+template void Split(const std::string& s, char separator, std::vector<std::string>* out_result);
+template void Split(const char *const& s, char separator, std::vector<std::string_view>* out_result);
+template void Split(const std::string_view& s,
+                    char separator,
+                    std::vector<std::string_view>* out_result);
+
+template <typename Str>
+void Split(const Str& s, char separator, size_t len, Str* out_result) {
+  Str* last = out_result + len;
+  auto split = SplitString(std::string_view(s), separator);
+  for (std::string_view p : split) {
+    if (p.empty()) {
+      continue;
+    }
+    if (out_result == last) {
+      return;
+    }
+    *out_result++ = Str(p);
+  }
+}
+
+template void Split(const std::string& s, char separator, size_t len, std::string* out_result);
+template void Split(const std::string_view& s,
+                    char separator,
+                    size_t len,
+                    std::string_view* out_result);
 
 void SetThreadName(const char* thread_name) {
   bool hasAt = false;

libartbase/base/utils.h
@@ -44,7 +44,16 @@ std::string PrettySize(int64_t size_in_bytes);
 
 // Splits a string using the given separator character into a vector of
 // strings. Empty strings will be omitted.
-void Split(const std::string& s, char separator, std::vector<std::string>* result);
+template<typename StrIn, typename Str>
+void Split(const StrIn& s, char separator, std::vector<Str>* out_result);
+
+template<typename Str>
+void Split(const Str& s, char separator, size_t len, Str* out_result);
+
+template<typename StrIn, typename Str, size_t kLen>
+void Split(const StrIn& s, char separator, std::array<Str, kLen>* out_result) {
+  Split<Str>(Str(s), separator, kLen, &((*out_result)[0]));
+}
 
 // Returns the calling thread's tid. (The C libraries don't expose this.)
 pid_t GetTid();